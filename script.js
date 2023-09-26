798c88f641305ce3d40d44457f5763071a7128ad
Miguel <miguelmpp12@gmail.com>
2023-09-26 09:00:47 -0300

    switc, incremento e decremento

---
+++ script.js
@@ -0,0 +1,30 @@
+/* switch
+
+let opcao = parseInt(prompt("Digite uma opçao: "));
+
+switch(opcao){
+    case 1:
+        document.write("Primeira Opção")
+        break;
+    case 2:
+        document.write("Segunda Opção")
+        break;
+    case 3: 
+        document.write("Terceira opção")
+        break;
+    default :
+        document.write("nehuma das opções validas")
+        break;
+
+}
+*/
+
+let valor = 7
+console.log(valor)
+valor = valor-1
+console.log(valor);
+
+let valor1 =10
+console.log(valor1)
+valor1 +=3;
+console.log(valor1)
